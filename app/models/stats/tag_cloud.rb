d481e3bc2af186d3d0f1b093efb1009609714af0
Simon Olsberg <simon@olsbergfamily.net>
2013-09-21 17:21:44 -0400

    extract sql query strings to private methods

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -15,16 +15,7 @@ class TagCloud
   def compute
     levels=10
 
-    query = "SELECT tags.id, name, count(*) AS count"
-    query << " FROM taggings, tags, todos"
-    query << " WHERE tags.id = tag_id"
-    query << " AND taggings.taggable_id = todos.id"
-    query << " AND todos.user_id="+user.id.to_s+" "
-    query << " AND taggings.taggable_type='Todo' "
-    query << " GROUP BY tags.id, tags.name"
-    query << " ORDER BY count DESC, name"
-    query << " LIMIT 100"
-    @tags = Tag.find_by_sql(query).sort_by { |tag| tag.name.downcase }
+    @tags = Tag.find_by_sql(sql).sort_by { |tag| tag.name.downcase }
 
     max, @min = 0, 0
     @tags.each { |t|
@@ -34,6 +25,34 @@ class TagCloud
 
     @divisor = ((max - @min) / levels) + 1
 
+    @tags_90days = Tag.find_by_sql(
+      [sql_90days, user.id, @cut_off, @cut_off]
+    ).sort_by { |tag| tag.name.downcase }
+
+    max_90days, @min_90days = 0, 0
+    @tags_90days.each { |t|
+      max_90days = [t.count.to_i, max_90days].max
+      @min_90days = [t.count.to_i, @min_90days].min
+    }
+
+    @divisor_90days = ((max_90days - @min_90days) / levels) + 1
+  end
+
+private
+
+  def sql
+    query = "SELECT tags.id, name, count(*) AS count"
+    query << " FROM taggings, tags, todos"
+    query << " WHERE tags.id = tag_id"
+    query << " AND taggings.taggable_id = todos.id"
+    query << " AND todos.user_id="+user.id.to_s+" "
+    query << " AND taggings.taggable_type='Todo' "
+    query << " GROUP BY tags.id, tags.name"
+    query << " ORDER BY count DESC, name"
+    query << " LIMIT 100"
+  end
+
+  def sql_90days
     query = "SELECT tags.id, tags.name AS name, count(*) AS count"
     query << " FROM taggings, tags, todos"
     query << " WHERE tags.id = tag_id"
@@ -45,16 +64,5 @@ class TagCloud
     query << " GROUP BY tags.id, tags.name"
     query << " ORDER BY count DESC, name"
     query << " LIMIT 100"
-    @tags_90days = Tag.find_by_sql(
-      [query, user.id, @cut_off, @cut_off]
-    ).sort_by { |tag| tag.name.downcase }
-
-    max_90days, @min_90days = 0, 0
-    @tags_90days.each { |t|
-      max_90days = [t.count.to_i, max_90days].max
-      @min_90days = [t.count.to_i, @min_90days].min
-    }
-
-    @divisor_90days = ((max_90days - @min_90days) / levels) + 1
   end
 end
