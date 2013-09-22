06b0dbafffe6ff3bb1d8634cc6cf725f823d6801
Simon Olsberg <simon@olsbergfamily.net>
2013-09-22 08:04:59 -0400

    remove all duplication in TagCloud and instead use 2 tag clouds

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -6,7 +6,7 @@ class TagCloud
   attr_reader :tags, :min, :divisor
   attr_reader :tags_90days, :min_90days, :divisor_90days
 
-  def initialize (user, cut_off)
+  def initialize (user, cut_off=nil)
     @user = user
     @cut_off = cut_off
   end
@@ -15,7 +15,10 @@ class TagCloud
   def compute
     levels=10
 
-    params = [sql, user.id]
+    params = [sql(@cut_off), user.id]
+    if @cut_off
+      params += [@cut_off, @cut_off]
+    end
     @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
 
     max, @min = 0, 0
@@ -25,17 +28,6 @@ class TagCloud
     }
 
     @divisor = ((max - @min) / levels) + 1
-
-    params = [sql(@cut_off), user.id, @cut_off, @cut_off]
-    @tags_90days = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
-
-    max_90days, @min_90days = 0, 0
-    @tags_90days.each { |t|
-      max_90days = [t.count.to_i, max_90days].max
-      @min_90days = [t.count.to_i, @min_90days].min
-    }
-
-    @divisor_90days = ((max_90days - @min_90days) / levels) + 1
   end
 
 private
